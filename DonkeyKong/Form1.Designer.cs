08f8d379d61a1bb8293c54f7595b825c7a2e880d
Alexis OTTINA <aott@sio.local>
2019-09-20 16:01:07 +0200

    DonkeyKong 0.011

--- DonkeyKong/Form1.Designer.cs
+++ DonkeyKong/Form1.Designer.cs
@@ -34,7 +34,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(656, 729);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- DonkeyKong/Form1.cs
+++ DonkeyKong/Form1.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 
 namespace DonkeyKong
 {
@@ -20,9 +22,13 @@ namespace DonkeyKong
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Size = new Size(224, 256);
-            //BackgroundImage = new Bitmap
-            Debug.WriteLine(Environment.CurrentDirectory);
+            string projectDir = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string stageImg = projectDir + @"\Assets\Sprites\Stage.png";
+
+            BackgroundImage = new Bitmap(stageImg);
+            BackgroundImageLayout = ImageLayout.Stretch;
+
+            MaximizeBox = false;
         }
     }
 }
